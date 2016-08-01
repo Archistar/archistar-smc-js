fe76bb9047ffd862acc1bbe92c0114c049dde3f4
Florian Wohner <florian.wohner@ait.ac.at>
2016-08-01 17:52:53 +0200

    Initial commit with simple grunt.js project (closes issue #1)

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('test', ['jshint', 'qunit']);
+};
