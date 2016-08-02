32aa8088202a2145a2b5d52773baa4601e4b4c09
Florian Wohner <florian.wohner@ait.ac.at>
2016-08-02 17:48:38 +0200

    translated the GF256 math-functions

---
+++ src/gf256.js
@@ -0,0 +1,48 @@
+/*
+this is a straight translation of the java class:
+https://github.com/Archistar/archistar-smc/blob/master/src/main/java/at/archistar/crypto/math/gf256/GF256.java
+
+the lookup tables from the original are pregenerated into a separate file
+*/
+var gf256 = gf256 || {};
+
+gf256.add = function (a, b) {
+  return a ^ b;
+}
+
+gf256.sub = function (a, b) {
+  return a ^ b;
+}
+
+gf256.mult = function (a, b) {
+ return gf256.alogtable(gf256.logtable[a] + gf256.logtable[b]);
+}
+
+gf256.pow = function (a, p) {
+  if (a === 0 && p !== 0) {
+    return 0;
+  } else {
+    return alogtable[p*logtable[a] % 255];
+  }
+}
+
+gf256.inverse = function (a) {
+ return alogtable[255 - (logtable[a] % 255)];
+}
+
+gf256.div = function (a, b) {
+  if (b === 0) {
+    throw "Division by Zero";
+  }
+  return alogtable[logtable[a] + 255 - logtable[b]];
+}
+
+gf256.evaluateAt = function (coeffs, x) {
+  var degree = coeffs.length - 1;
+  var result = coeffs[degree];
+
+  for (var i = degree - 1; i >= 0; i--){
+    result = add(mult(result, x), coeffs[i]);
+  }
+  return result;
+}
