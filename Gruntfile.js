928441dfc22d137347d464c57a5a84352ecbc16e
Florian Wohner <florian.wohner@ait.ac.at>
2016-08-02 16:44:48 +0200

    generated lookup tables for GF256

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,11 @@ module.exports = function(grunt) {
     qunit: {
       files: ['test/**/*.html']
     },
+    generate: {
+      options: {
+        dest: 'src'
+      }
+    },
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
@@ -16,7 +21,13 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-generate');
   grunt.loadNpmTasks('grunt-contrib-watch');
+
+  //in case we ever need to regenerate the lookup tables:
+  //uncomment the following two lines and do "grunt generate:lookup:table"
+  //grunt.config.set('logtable', eval(grunt.file.read('scripts/generate_lookup_tables.js'))[0].toString());
+  //grunt.config.set('alogtable', eval(grunt.file.read('scripts/generate_lookup_tables.js'))[1].toString());
 
   grunt.registerTask('default', ['jshint']);
   grunt.registerTask('test', ['jshint', 'qunit']);
---
+++ templates/lookup.js
@@ -0,0 +1,5 @@
+var gf256 = gf256 || {};
+
+gf256.logtable = [<%= grunt.config.get('logtable') %>];
+
+gf256.alogtable = [<%= grunt.config.get('alogtable') %>];
