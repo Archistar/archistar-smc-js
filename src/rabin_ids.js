303e3fbe47b60daddd0438dd3e29ac7c70e892d2
Florian Wohner <florian.wohner@ait.ac.at>
2016-08-11 12:47:17 +0200

    nodeify rabin ids

--- src/rabin_ids.js
+++ src/rabin_ids.js
@@ -4,7 +4,10 @@ code is mostly taken from the Java version:
 https://github.com/Archistar/archistar-smc/blob/master/src/main/java/at/archistar/crypto/secretsharing/RabinIDS.java
 */
 
-var rabin_ids = rabin_ids || {};
+var rabin_ids = module.exports;
+
+const gf256 = require('./gf256.js');
+const matrix = require('./matrix.js');
 
 // random is a source that implements the functionality of window.crypto.getRandomValues()
 // if it is undefined, window.crypto.getRandomValues() will be used
@@ -13,14 +16,14 @@ rabin_ids.Configuration = function (shares, quorum) {
   this.quorum = quorum;
   this.encode = function (secret) {
     'use strict';
-    var chunks = Math.ceil(secret.length / this.quorum);
-    var shs = [];
-    for (var k = 0; k < this.shares; k++) {shs[k] = {data: [], degree: k + 1, original_length: secret.length};}
-    var i = 0;
+    const chunks = Math.ceil(secret.length / this.quorum);
+    const shs = [];
+    for (let k = 0; k < this.shares; k++) {shs[k] = {data: [], degree: k + 1, original_length: secret.length};}
+    let i = 0;
 
-    for (var chunk = 0; chunk < chunks; chunk++) {
-      var coeffs = [];
-      for (var j = 0; j < this.quorum; j++) {
+    for (let chunk = 0; chunk < chunks; chunk++) {
+      const coeffs = [];
+      for (let j = 0; j < this.quorum; j++) {
         if (secret[i] !== undefined) {
           coeffs[j] = secret[i];
           i++;
@@ -28,7 +31,7 @@ rabin_ids.Configuration = function (shares, quorum) {
           coeffs[j] = 0;
         }
       }
-      for (var n = 0; n < this.shares; n++) {
+      for (let n = 0; n < this.shares; n++) {
         shs[n].data[chunk] = gf256.evaluateAt(coeffs, n + 1);
       }
     }
@@ -38,19 +41,19 @@ rabin_ids.Configuration = function (shares, quorum) {
   this.decode = function (shs) {
     'use strict';
     var xvalues = new Uint8Array(shs.length);
-    for (var i0 = 0; i0 < shs.length; i0++) {xvalues[i0] = shs[i0].degree;}
-    var decoder = matrix.generate_decoder(this.quorum, xvalues);
-    var secret = [];
-    var length = shs[0].data.length;
-    var original_length = shs[0].original_length;
-    for (var i = 0; i < length; i++) {
-      var yvalues = [];
-      for (var i2 = 0; i2 < this.quorum; i2++) {yvalues[i2] = shs[i2].data[i];}
-      var decoded = matrix.multiply_vector(decoder, yvalues);
+    for (let i0 = 0; i0 < shs.length; i0++) {xvalues[i0] = shs[i0].degree;}
+    const decoder = matrix.generate_decoder(this.quorum, xvalues);
+    let secret = [];
+    const length = shs[0].data.length;
+    const original_length = shs[0].original_length;
+    for (let i = 0; i < length; i++) {
+      const yvalues = [];
+      for (let i2 = 0; i2 < this.quorum; i2++) {yvalues[i2] = shs[i2].data[i];}
+      const decoded = matrix.multiply_vector(decoder, yvalues);
       if (secret.length + decoded.length < original_length) {
         secret = secret.concat(decoded);
       } else {
-        for (var i1 = 0; secret.length < original_length; i1++) {
+        for (let i1 = 0; secret.length < original_length; i1++) {
           secret.push(decoded[i1]);
         }
         break;
---
+++ test/rabin_ids.js
@@ -0,0 +1,43 @@
+exports.roundtrip1 = function(test) {
+  'use strict';
+  test.expect(2);
+  const rabin_ids = require('./../src/rabin_ids.js');
+  const rabin = new rabin_ids.Configuration(10, 6);
+  const text = new Uint8Array([2,3,56,32,57,124,45,34,98,61,1,9,123,233,210]);
+  const res = rabin.encode(text);
+  test.deepEqual(rabin.decode(res), text);
+  test.deepEqual(rabin.decode([res[5],res[3],res[8],res[0],res[2],res[6]]), text);
+  test.done();
+};
+
+exports.roundtrip2 = function(test) {
+  'use strict';
+  test.expect(2);
+  const rabin_ids = require('./../src/rabin_ids.js');
+  const rabin = new rabin_ids.Configuration(10, 6);
+  const text = new Uint8Array([2,3,56,32,57,124,45,34,98,61,1,9,123,233,210,198]);
+  const res = rabin.encode(text);
+  test.deepEqual(rabin.decode(res), text);
+  test.deepEqual(rabin.decode([res[5],res[3],res[8],res[0],res[2],res[6]]), text);
+  test.done();
+};
+
+exports.compare_with_java = function(test) {
+  'use strict';
+  test.expect(10);
+  const rabin_ids = require('./../src/rabin_ids.js');
+  const rabin = new rabin_ids.Configuration(10, 6);
+  const text = new Uint8Array([2,3,56,32,57,124,45,34,98,61,1,9,123,233,210,198]);
+  const res = rabin.encode(text);
+  test.deepEqual(res[0].data, [92, 88, 134]);
+  test.deepEqual(res[1].data, [117, 36, 165]);
+  test.deepEqual(res[2].data, [128, 93, 246]);
+  test.deepEqual(res[3].data, [205, 222, 142]);
+  test.deepEqual(res[4].data, [1, 121, 176]);
+  test.deepEqual(res[5].data, [21, 94, 73]);
+  test.deepEqual(res[6].data, [114, 245, 217]);
+  test.deepEqual(res[7].data, [190, 200, 40]);
+  test.deepEqual(res[8].data, [30, 16, 124]);
+  test.deepEqual(res[9].data, [25, 230, 160]);
+  test.done();
+};
