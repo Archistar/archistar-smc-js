8dd16223a4dd2b6bdfc3a0875a3f4a4a7a47f6d2
Florian Wohner <florian.wohner@ait.ac.at>
2016-08-11 14:43:27 +0200

    nodeify krawczyk

--- src/krawczyk_css.js
+++ src/krawczyk_css.js
@@ -2,34 +2,43 @@
 an implementation of Krawczyk's Computationally Secure Secret Sharing
 http://courses.csail.mit.edu/6.857/2009/handouts/short-krawczyk.pdf
 */
-var krawczyk_css = krawczyk_css || {};
+var krawczyk_css = module.exports;
 
 krawczyk_css.Configuration = function (shares, quorum, random) {
   this.shares = shares;
   this.quorum = quorum;
   this.random = random;
+  const rabin_ids = require('./rabin_ids.js');
   this.rabin = new rabin_ids.Configuration(shares, quorum);
+  const shamir_pss = require('./shamir_pss.js');
   this.shamir = new shamir_pss.Configuration(shares, quorum, random);
+  const salsa20 = require('./salsa20.js');
   this.encode = function (secret) {
-    var key_nonce = new Uint8Array(40);
-    window.crypto.getRandomValues(key_nonce);
-    console.log(key_nonce);
-    var encrypted_secret = salsa20.code(key_nonce.slice(0,32), key_nonce.slice(32,40), secret);
-    var shs = this.rabin.encode(encrypted_secret);
-    var key_nonce_shares = this.shamir.encode(key_nonce);
-    for (var i = 0; i < this.shares; i++) {
+    'use strict';
+    let key_nonce = new Uint8Array(40);
+    if (typeof window == 'function') {
+      window.crypto.getRandomValues(key_nonce);
+    } else {
+      const crypto = require('crypto');
+      key_nonce = crypto.randomBytes(40);
+    }
+    const encrypted_secret = salsa20.code(key_nonce.slice(0,32), key_nonce.slice(32,40), secret);
+    const shs = this.rabin.encode(encrypted_secret);
+    const key_nonce_shares = this.shamir.encode(key_nonce);
+    for (let i = 0; i < this.shares; i++) {
       shs[i].key_nonce_data = key_nonce_shares[i];
     }
     return shs;
   };
   this.decode = function (shs) {
-    var key_nonce_data = new Array(shs.length);
-    for (var i = 0; i < shs.length; i++) {
+    'use strict';
+    const key_nonce_data = new Array(shs.length);
+    for (let i = 0; i < shs.length; i++) {
       key_nonce_data[i] = shs[i].key_nonce_data;
     }
-    var key_nonce = this.shamir.decode(key_nonce_data);
-    var encrypted_text = this.rabin.decode(shs);
-    var decrypted_text = salsa20.code(key_nonce.slice(0,32), key_nonce.slice(32,40), encrypted_text);
+    const key_nonce = this.shamir.decode(key_nonce_data);
+    const encrypted_text = this.rabin.decode(shs);
+    const decrypted_text = salsa20.code(key_nonce.slice(0,32), key_nonce.slice(32,40), encrypted_text);
     return decrypted_text;
   };
 };
---
+++ test/krawczyk_css.js
@@ -0,0 +1,13 @@
+exports.roundtrip1 = function(test) {
+  'use strict';
+  test.expect(1);
+  const krawczyk_css = require('./../src/krawczyk_css.js');
+  const fake_rng = require('./../src/fake_rng.js');
+  const crypto = require('crypto');
+  const krawczyk = new krawczyk_css.Configuration(10, 6, fake_rng.get_values_4);
+  const text = crypto.randomBytes(4096);
+  const encoded = krawczyk.encode(text);
+  const decoded = krawczyk.decode(encoded);
+  test.deepEqual(text, decoded);
+  test.done();
+};
