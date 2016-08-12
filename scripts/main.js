752a26adaae67aca0213b9b38f5686ebb5f136cb
Florian Wohner <florian.wohner@ait.ac.at>
2016-08-12 18:45:13 +0200

    GUI progress (make dynamic; enable selection of shares)

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,54 @@
 share = function() {
+  const shares = new Number(document.getElementById(1).value);
+  const quorum = new Number(document.getElementById(2).value);
+  if (quorum >= shares) {alert("number shares must be greater than quorum for reconstruction"); return;}
+
   const input = document.getElementById(0);
-  alert(input.value);
   const text = require('./../src/text.js');
   const krawczyk_css = require('./../src/krawczyk_css.js');
-  const krawczyk = new krawczyk_css.Configuration(10, 6);
+  const krawczyk = new krawczyk_css.Configuration(shares, quorum);
   const encoded = krawczyk.encode(text.string_to_byte_array(input.value));
 
-  for (let i = 0; i < encoded.length; i++) {
-    const heading = document.createElement("h3");
-    const heading_text = document.createTextNode(encoded[i].data);
-    heading.appendChild(heading_text);
-    document.body.appendChild(heading);
+  const paragraph = document.getElementById(3);
+  while (paragraph.firstChild) {
+    paragraph.removeChild(paragraph.firstChild);
   }
+
+  for (let i = 0; i < encoded.length; i++) {
+    const p = document.createElement("p");
+    const data = document.createTextNode(encoded[i].data);
+    const keynonce = document.createTextNode(encoded[i].key_nonce_data.data[0]);
+    const degree = document.createTextNode(encoded[i].key_nonce_data.degree);
+    const b = document.createElement("input");
+    b.type = "checkbox";
+    p.appendChild(b);
+    p.appendChild(data);
+    p.appendChild(keynonce);
+    p.appendChild(degree);
+    paragraph.appendChild(p);
+  }
+
+};
+
+recombine = function() {
+  const par = document.getElementById(3);
+  const list = [];
+
+  const shares = new Number(document.getElementById(1).value);
+  const quorum = new Number(document.getElementById(2).value);
+
+  for (let i = 0; i < shares; i++) {
+    if (par.children[i].childNodes[0].checked) {
+      list.push(par.children[i].childNodes[1]);
+    }
+  }
+
+  if (list.length < quorum) {
+    alert("not enough shares selected");
+    return;
+  }
+
+  const krawczyk_css = require('./../src/krawczyk_css.js');
+  const krawczyk = new krawczyk_css.Configuration(shares, quorum);
 
 };
