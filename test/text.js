a7323aad549a87f0851b74235f05be97851a0894
Florian Wohner <florian.wohner@ait.ac.at>
2016-08-22 12:45:54 +0200

    forgot to add test data

--- test/text.js
+++ test/text.js
@@ -3,7 +3,7 @@ exports.test = function(test) {
   test.expect(1);
   const text = require('./../src/text.js');
   const str = text.string_to_byte_array("Write something here ÖÄÜß€");
-  test.deepEqual(str, new Uint8Array([87, 114, 105]));
+  test.deepEqual(str, new Uint8Array([0, 87, 0, 114, 0, 105, 0, 116, 0, 101, 0, 32, 0, 115, 0, 111, 0, 109, 0, 101, 0, 116, 0, 104, 0, 105, 0, 110, 0, 103, 0, 32, 0, 104, 0, 101, 0, 114, 0, 101, 0, 32, 0, 214, 0, 196, 0, 220, 0, 223, 32, 172]));
   test.done();
 };
 
