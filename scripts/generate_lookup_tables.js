4a88720901ee1e52c16418e8320b0155e437d401
Florian Wohner <florian.wohner@ait.ac.at>
2017-11-10 18:23:04 +0100

    some linting on the script for generating lookup tables

--- scripts/generate_lookup_tables.js
+++ scripts/generate_lookup_tables.js
@@ -6,16 +6,16 @@ https://github.com/Archistar/archistar-smc/blob/master/src/main/java/at/archista
 the idea is to use it as input for a template that generates a .js file
 */
 
-var logtable = new Int16Array(256);
-var alogtable = new Int16Array(1025);
-var genpoly = 0x11D;
+let logtable = new Int16Array(256);
+let alogtable = new Int16Array(1025);
+let genpoly = 0x11D;
 
 logtable[0] = 512;
 alogtable[0] = 1;
 
-for (var i = 1; i < 255; i++) {
-  var next = (alogtable[i-1])*2;
-  if (next >= 256) {next = next ^ genpoly};
+for (let i = 1; i < 255; i++) {
+  let next = (alogtable[i-1])*2;
+  if (next >= 256) {next = next ^ genpoly;}
 
   alogtable[i] = next;
   logtable[alogtable[i]] = i;
@@ -24,14 +24,14 @@ for (var i = 1; i < 255; i++) {
 alogtable[255] = alogtable[0];
 logtable[alogtable[255]] = 255;
 
-for (var i = 256; i < 510; i++) {
+for (let i = 256; i < 510; i++) {
   alogtable[i] = alogtable[i % 255];
 }
 
 alogtable[510] = 1;
 
-for (var i = 511; i < 1020; i++) {
+for (let i = 511; i < 1020; i++) {
   alogtable[i] = 0;
 }
 
-[logtable, alogtable]
+[logtable, alogtable];
