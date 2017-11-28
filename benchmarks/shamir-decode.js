18b7776b205c3961421daecd222500ed835bc28d
Florian Wohner <florian.wohner@ait.ac.at>
2017-11-28 17:11:18 +0100

    fix shamir-decode benchmark

--- benchmarks/shamir-decode.js
+++ benchmarks/shamir-decode.js
@@ -11,22 +11,22 @@ module.exports = {
   tests: [
     {
       name: '(4/3):    4KB',
-      setup: function() {const engine = setup(4,3); const text = engine.encode(randomText(4096));},
+      setup: function() {const text = engine.encode(randomText(4096));},
       fn: function() {return engine.decode(text);}
     },
     {
       name: '(4/3):  128KB',
-      setup: function() {const engine = setup(4,3); const text = engine.encode(randomText(131072));},
+      setup: function() {const text = engine.encode(randomText(131072));},
       fn: function() {return engine.decode(text);}
     },
     {
       name: '(4/3):  512KB',
-      setup: function() {const engine = setup(4,3); const text = engine.encode(randomText(524288));},
+      setup: function() {const text = engine.encode(randomText(524288));},
       fn: function() {return engine.decode(text);}
     },
     {
       name: '(4/3): 4096KB',
-      setup: function() {const engine = setup(4,3); const text = engine.encode(randomText(4194304));},
+      setup: function() {const text = engine.encode(randomText(4194304));},
       fn: function() {return engine.decode(text);}
     }
   ]
